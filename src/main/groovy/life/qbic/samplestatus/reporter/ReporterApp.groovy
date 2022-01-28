70caeb2aadeaec383471752a9f96b792c79a6f9b
Tobias Koch <KochTobi@users.noreply.github.com>
2022-01-28 16:26:48 +0100

    Update since tag
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- src/main/groovy/life/qbic/samplestatus/reporter/ReporterApp.groovy
+++ src/main/groovy/life/qbic/samplestatus/reporter/ReporterApp.groovy
@@ -17,7 +17,7 @@ import picocli.CommandLine
  *
  * <p>The entry-point of the reporter application.</p>
  *
- * @since 0.1.0
+ * @since 1.0.0
  */
 @SpringBootApplication
 class ReporterApp implements CommandLineRunner {
